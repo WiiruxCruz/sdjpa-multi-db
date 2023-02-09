c888b63edd3dee9247dc76f3fe9ecdf1ac74f7c8
WiiruxCruz <hialf.92@gmail.com>
2023-02-09 10:32:27 -0600

    Adding project lombok

--- src/main/java/guruspringframework/sdjpamultidb/domain/creditcard/CreditCard.java
+++ src/main/java/guruspringframework/sdjpamultidb/domain/creditcard/CreditCard.java
@@ -2,8 +2,18 @@ package guruspringframework.sdjpamultidb.domain.creditcard;
 
 import guruspringframework.sdjpamultidb.domain.CreditCardConverter;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreditCard {
 
     @Id
